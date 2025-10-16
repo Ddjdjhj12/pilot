ba7931a8c45b7eb69bf8e49ed2d5bdc265d64ef6
Ddjdjhj12 <h18293628807@gmail.com>
2025-10-16 14:54:22 +0800

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -3,7 +3,7 @@ import "@fontsource-variable/cabin";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import type { SeoConfig } from "@shopify/hydrogen";
 import { Analytics, getSeoMeta, useNonce } from "@shopify/hydrogen";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import type {
   LinksFunction,
   LoaderFunctionArgs,
